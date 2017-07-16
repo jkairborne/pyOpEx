ed07566b41582da53f5fb1ca84d04e2ced2146c0
jk <jk@airsensing.com>
2017-07-15 22:49:21 -0400

    created uart_write_to_video, which allows us to record video with the Roll and pitch info. Now need to think of a proper way to test this to get the lag as accurately as possible

--- uart_write_to_video.py
+++ uart_write_to_video.py
@@ -9,10 +9,10 @@ import sensor, image, time, mjpeg, pyb
 class RPReceiver:
     def __init__(self,expectedlength=6, stchar = 0x58): #0x58 corresponds to 'X'
         self.uart = UART(3, 57600)
-        self.uart.init(57600, bits=8, parity=None, stop=1,timeout=1000, flow=0, timeout_char=0, read_buf_len=300)
+        self.uart.init(57600, bits=8, parity=None, stop=1,timeout=1000, flow=0, timeout_char=0, read_buf_len=10000)
         self.startbyte = stchar
         self.xlength = expectedlength
-        self.buf = bytearray(300)
+        self.buf = bytearray(10000)
         self.numbytes =0
         self.potmsg = bytearray(self.xlength)
         self.roll = 0
@@ -79,15 +79,42 @@ def verify_checksum(data):
         print("checksum failed!")
         return False
 
+
+RED_LED_PIN = 1
+BLUE_LED_PIN = 3
+
+sensor.reset() # Initialize the camera sensor.
+sensor.set_pixformat(sensor.RGB565) # or sensor.GRAYSCALE
+sensor.set_framesize(sensor.QVGA) # or sensor.QQVGA (or others)
+clock = time.clock() # Tracks FPS.
+
+
+
 if __name__=="__main__":
     print("at the start")
     rec = RPReceiver()
-    while(True):
-        rec.sync()
-        [tst,tst1] = rec.getrp()
-        print("roll and pitch: %f   %f " % (tst,tst1))
-        time.sleep(500)
+    pyb.LED(RED_LED_PIN).on()
+    sensor.skip_frames(time = 2000) # Give the user time to get ready.
 
+    pyb.LED(RED_LED_PIN).off()
+    pyb.LED(BLUE_LED_PIN).on()
+    m = mjpeg.Mjpeg("example9.mjpeg")
+    for i in range(150):
+        clock.tick()
+        image = sensor.snapshot()
+       # if(i>10):
+        rec.sync()
+        [rll,ptc] = rec.getrp()
+        image.draw_string(0, 0, "data from UART: %.2f %.2f"%(rll,ptc), color = (0xFF, 0x00, 0x00))
+        print("roll, pitch: %.2f %.2f" % (rll,ptc))
+        m.add_frame(image)
+        print("frame #%d" % i)
+        print(clock.fps())
+
+
+    m.close(clock.fps())
+    pyb.LED(BLUE_LED_PIN).off()
+    print("Done! Reset the camera to see the saved recording.")
 
 '''
 
