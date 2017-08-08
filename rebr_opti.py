34141ec94425d9a03e8999a0af5eaa02803cef71
jk <jk@airsensing.com>
2017-08-08 16:16:15 -0400

    This version of rebr_opti is actually a lightly modified version of the previous commit's 2rebr_opti.py - it is now working to accept data from optitrack and send it to pixracer at 57600 baud. Still need to get it working on an Xbee

--- rebr_opti.py
+++ rebr_opti.py
@@ -1,90 +1,37 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 
-"""Class for writing position controller."""
+from __future__ import print_function
 
-from __future__ import division, print_function, absolute_import
-
-# Import ROS libraries
+import time
 import roslib
 import rospy
-import numpy as np
-import math
-import struct
-from math import sin, cos, pi, asin
 
-import mavlink
 from pymavlink import mavutil
-import serial, string, math, time, calendar
-# Import class that computes the desired positions
+
 from tf.transformations import euler_from_quaternion
 from geometry_msgs.msg import TransformStamped, Twist, TwistStamped, PoseStamped
 
+from argparse import ArgumentParser
+parser = ArgumentParser(description=__doc__)
 
-packet_sequence = 0
+parser.add_argument("--baudrate", type=int,
+                  help="master port baud rate", default=115200)
+parser.add_argument("--device", required=True, help="serial device", default="/dev/ttyACM0")
+args = parser.parse_args()
 
-def checksum(data, extra): # https://github.com/mavlink/c_library_v1/blob/master/checksum.h
-    output = 0xFFFF
-    for i in range(len(data)):
-        tmp = data[i] ^ (output & 0xFF)
-        tmp = (tmp ^ (tmp << 4)) & 0xFF
-        output = ((output >> 8) ^ (tmp << 8) ^ (tmp << 3) ^ (tmp >> 4)) & 0xFFFF
-    tmp = extra ^ (output & 0xFF)
-    tmp = (tmp ^ (tmp << 4)) & 0xFF
-    output = ((output >> 8) ^ (tmp << 8) ^ (tmp << 3) ^ (tmp >> 4)) & 0xFFFF
-    return output
-
-def message_checksum(msg):
-    '''calculate a 8-bit checksum of the key fields of a message, so we
-       can detect incompatible XML changes'''
-    crc = mavutil.x25crc(msg.name + ' ')
-    for f in msg.ordered_fields:
-        crc.accumulate(f.type + ' ')
-        crc.accumulate(f.name + ' ')
-        if f.array_length:
-            crc.accumulate(chr(f.array_length))
-    return (crc.crc&0xFF) ^ (crc.crc>>8) 
+# create a mavlink serial instance
 
 
+t1 = time.time()
 
-MAV_target_component_id = 1
-MAV_MOCAP_extra_crc = 109
-MAV_MOCAP_message_id = 138
-MAV_system_id = 1
-MAV_component_id = 0x54
+counts = {}
 
-def send_att_pos_mocap_packet(quat,x,y,z):
-    global packet_sequence
-    temp = struct.pack('<Q4ffff',
-                       0,
-                       quat[0],
-                       quat[1],
-                       quat[2],
-                       quat[3],
-                       x,
-                       y,
-                       z)
-    temp = struct.pack("<bbbbB36s",
-                       36,
-                       0,#packet_sequence & 0xFF,
-                       MAV_system_id,
-                       MAV_component_id,
-                       MAV_MOCAP_message_id,
-                       temp)
-    #print(struct.calcsize(temp))
-    print(struct.calcsize("bbbbBQ4ffff"))
-    
-    temp = struct.pack("<B41sh",
-                       0xFE,
-                       temp,
-                       message_checksum(temp))#struct.unpack("41s",temp), MAV_MOCAP_extra_crc))
-    packet_sequence += 1
-    return temp
 
 
 class PositionController(object):
     def __init__(self):
         self.model_name = 'Track1'
-        self.ser = serial.Serial('/dev/ttyUSB0',921600)
+        self.master = mavutil.mavlink_connection(args.device, baud=args.baudrate)
 
         # Containers
         self.optitrack_data = PoseStamped() # container to store incoming optitrack data
@@ -107,7 +54,7 @@ class PositionController(object):
         quaternion = (self.optitrack_data.pose.orientation.x,self.optitrack_data.pose.orientation.y,self.optitrack_data.pose.orientation.z,self.optitrack_data.pose.orientation.w)
         (self.phi,self.theta,self.psi) = euler_from_quaternion(quaternion)
         print("x: %.2f, y: %.2f, z: %.2f, roll: %f, pitch:  %f, yaw:  %f" %(self.x, self.y, self.z,self.phi,self.theta,self.psi) )
-        self.ser.write(send_att_pos_mocap_packet(quaternion,self.x,self.y,self.z))
+        self.master.mav.att_pos_mocap_send(0, quaternion,self.x,self.y,self.z)
         
         #SEND DATA OVER SERIAL
 
