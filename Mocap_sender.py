43aed3ce9720d140c3edaec25337fca23bdaf58e
jk <jk@airsensing.com>
2017-08-14 13:38:47 -0400

    Turns out the error must have been in the way messages were defined? And possibly me forgetting to change baud rates. It appears that Mocap_sender.py works just fine. Removed MsgSender.py which seemed to cause issues

--- Mocap_sender.py
+++ Mocap_sender.py
@@ -97,7 +97,7 @@ while(True):
     clock.tick()
     time.sleep(100)
     send_att_pos_mocap_packet(uart,[1,0,0,0],11,22,33)
-   # send_set_position_target_local_ned_packet(uart,-10,-20,-30,1.2)
+    send_set_position_target_local_ned_packet(uart,-10,-20,-30,1.2)
    # print("in loop")
    # print("FPS %f" % clock.fps())
     count +=1
