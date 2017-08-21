36f917371d4255977ff4d63f86c79803646bb2bc
jk <jk@airsensing.com>
2017-08-21 11:18:49 -0400

    Changing computers

--- temp/plotBagfile.sh
+++ temp/plotBagfile.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
 gnuplot -persist <<-EOFMarker
-    print "-"
     set autoscale
     set datafile separator ","
     set key autotitle columnhead
     plot "bagfile-_cmd_vel.csv" using (\$1):2 with linespoints
     first=GPVAL_DATA_X_MIN
-    plot "bagfile-_img_data.csv" using (\$1):2 with linespoints
+    plot "bagfile-_img_data.csv" using (\$1):3 with linespoints
     second=GPVAL_DATA_X_MIN
-    print(first)
-    print(second)
-    plot "bagfile-_cmd_vel.csv" using ((\$1-first)/10**9):2, "bagfile-_img_data.csv" using ((\$1-second)/10**9):3
+    plot "bagfile-_cmd_vel.csv" using ((\$1-first)/10**9):2, "bagfile-_img_data.csv" using ((\$1-second)/10**9):((\$3-180)/180)
 EOFMarker
 
 # rest of script, after gnuplot exits
