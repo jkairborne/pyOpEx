7cf31cf8cb685b6db9cfbb86523092c97b8821e6
jk <jk@airsensing.com>
2017-08-21 16:19:26 -0400

    transfer computers

--- temp/plotBagfile.sh
+++ temp/plotBagfile.sh
@@ -22,3 +22,22 @@ gnuplot -persist <<-EOFMarker
     plot "bagfile-_cmd_vel.csv" using ((\$1-first)/10**9):3, "bagfile-_img_data.csv" using ((\$1-first)/10**9):((\$2-360)/720)
 EOFMarker
 # rest of script, after gnuplot exits
+gnuplot -persist <<-EOFMarker
+    set autoscale
+    set datafile separator ","
+    set key autotitle columnhead
+    set grid
+    plot "bagfile-_vrpn_client_node_ardrone_pose.csv" using (\$1):5 with linespoints
+    first=GPVAL_DATA_X_MIN
+    plot "bagfile-_vrpn_client_node_ardrone_pose.csv" using ((\$1-first)/10**9):7, "bagfile-_vrpn_client_node_target_pose.csv" using ((\$1-first)/10**9):7
+EOFMarker
+gnuplot -persist <<-EOFMarker
+    set autoscale
+    set datafile separator ","
+    set yrange [-0.5:0.5]
+    set key autotitle columnhead
+    set grid
+    plot "bagfile-_vrpn_client_node_ardrone_pose.csv" using (\$1):2 with linespoints
+    first=GPVAL_DATA_X_MIN
+    plot "bagfile-_vrpn_client_node_ardrone_pose.csv" using ((\$1-first)/10**9):5, "bagfile-_vrpn_client_node_target_pose.csv" using ((\$1-first)/10**9):5
+EOFMarker
