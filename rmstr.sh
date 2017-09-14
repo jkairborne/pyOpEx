e2e7739a365d94d8a08b3bdfa789b16d31c4fdda
jk <jk@airsensing.com>
2017-09-14 19:57:06 -0400

    More experimental results. Without strings this time

--- rmstr.sh
+++ rmstr.sh
@@ -3,9 +3,14 @@ sed -i -e 's/_base_link//g' *
 sed -i -e 's/_base_footprint//g' *
 sed -i -e 's/_base_frontcam//g' *
 sed -i -e 's/_base_bottomcam//g' *
+sed -i -e 's/_wheel_link//g' *
 sed -i -e 's/,world//g' *
 sed -i -e 's/,ardrone//g' *
 sed -i -e 's/,roomba//g' *
 sed -i -e 's/,target//g' *
 sed -i -e 's/,odom//g' *
+sed -i -e 's/,left//g' *
+sed -i -e 's/,right//g' *
 
+sed -i -e 's/base_link//g' *
+sed -i -e 's/base_footprint//g' *
